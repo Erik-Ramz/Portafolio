65f300bebb910c01f31f46206d69cd3da8190712
Erik Ramirez <danielrmzmares@gmail.com>
2023-10-22 15:27:54 -0600

    Página finalizada

--- index.php
+++ index.php
@@ -1,3 +1,269 @@
-<?php
-require_once 'controller/ctr_master.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Portafolio</title>
+
+    <!-- Favicon -->
+    <link href="view/img/Logo.png" rel="icon">
+
+    <!-- Google Font -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400&family=Roboto:wght@400;500;700&display=swap" rel="stylesheet"> 
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="view/lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="view/lib/tempusdominus/css/tempusdominus-bootstrap-4.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="view/css/style.css" rel="stylesheet">
+
+    <!-- Model viewer -->
+    <script type="module" src="https://ajax.googleapis.com/ajax/libs/model-viewer/3.3.0/model-viewer.min.js"></script>
+</head>
+
+<body>
+    <!-- Navbar Start -->
+    <div class="container-fluid p-0 nav-bar">
+        <nav class="navbar navbar-expand-lg bg-none navbar-dark py-3 " style="background: rgba(0, 0, 0, 0.65);">
+            <a class="navbar-brand px-lg-4 m-0" >
+                <h1 class="m-0 text-white" style="text-shadow: 5px 1px 2px black">Erik Daniel Ramírez Mares</h1>
+                <h4 class="text-primary text-uppercase" style="letter-spacing: 5px; text-shadow: 5px 1px 2px black; font-size:30px">- Portafolio -</h4>
+            </a>
+            <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                <div class="navbar-nav ml-auto p-4">
+                    
+            </div>
+        </nav>
+    </div>
+    <!-- Navbar End -->
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0 mb-5">
+        <div id="blog-carousel" class="carousel slide overlay-bottom" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                <img class="w-100" src="view/img/Carrusel_1.png" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center" style="background: rgba(226, 51, 120, 0.3);">
+                        <h1 class="display-4 text-white m-0" style="text-shadow: 5px 1px 2px black">Edición de video</h1>
+                        <h2 class="text-white m-0" style="text-shadow: 5px 1px 2px black">Creación de contenido digital</h2>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="w-100" src="view/img/Carrusel_2.png" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center" style="background: rgba(238, 125, 47, 0.3);">
+                        <h1 class="display-4 text-white m-0" style="text-shadow: 5px 1px 2px black">Modelado 3D</h1>
+                        <h2 class="text-white m-0" style="text-shadow: 5px 1px 2px black">Cración de objetos y animación 3D</h2>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="w-100" src="view/img/Carrusel_3.png" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center" style="background: rgba(34, 175, 100, 0.3);">
+                        <h1 class="display-4 text-white m-0" style="text-shadow: 5px 1px 2px black">Programación web</h1>
+                        <h2 class="text-white m-0" style="text-shadow: 5px 1px 2px black">Desarrollo y diseño de sitios web</h2>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img class="w-100" src="view/img/Carrusel_4.png" alt="Image">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center" style="background: rgba(49, 38, 73, 0.45);">
+                        <h1 class="display-4 text-white m-0" style="text-shadow: 5px 1px 2px black">Programación en Unity</h1>
+                        <h2 class="text-white m-0" style="text-shadow: 5px 1px 2px black">Desarrollo y programación de videojuegos 3D y aplicaciones de realidad virtual</h2>
+                    </div>
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#blog-carousel" data-slide="prev">
+                <span class="carousel-control-prev-icon"></span>
+            </a>
+            <a class="carousel-control-next" href="#blog-carousel" data-slide="next">
+                <span class="carousel-control-next-icon"></span>
+            </a>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+    <!-- Sitio web Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="section-title">
+                <h1 class="display-4">Mis proyectos</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 py-0 py-lg-4">
+                    <h1 class="mb-3">Edición de video</h1>
+                    <h5 class="mb-3">Mi experiencia en el uso de diferentes herramientas como DaVinci Resolve, After Effects e Illustrator me permite crear contenido visual de alta calidad</h5>
+                    <p>Aquí podrás visualizar algunos proyectos que he aplicado mis habilidades en estos software para crear contenido visual atractivo para la vista</p>
+                </div>
+                <div class="col-lg-4 py-0 py-lg-4">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/ySZMZpI-oSk?si=xw5R1-UOuENg6KLA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Sitio web End -->
+    
+    <!-- Modelo 3d Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="position-relative overlay-top overlay-bottom" style="background: rgba(57, 49, 36, 1);">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 my-5 my-lg-0" >
+                        <div class="p-5">
+                        <style>
+                        model-viewer {
+                        width: 525px;
+                        height: 350px;
+                        }
+                        </style>
+                            <model-viewer class=" mx-auto d-block" src="view/assets/Jugete.glb" shadow-intensity="1" ar camera-controls touch-action="pan-y" alt="A 3D model carousel"></model-viewer>
+                            <div class="btn-toolbar" role="toolbar" aria-label="Toolbar with button groups">
+                                <div class="btn-group me-2 d-grid gap-2 col-6 mx-auto" role="group" aria-label="First group">
+                                <button class="slide service-icon btn btn-primary" onclick="switchSrc(this, 'Candelabro')"><i class="fa-solid fa-hanukiah"></i></button>
+                                <button class="slide service-icon btn btn-primary" onclick="switchSrc(this, 'CocinaIntegral')"><i class="fa-solid fa-fire-burner"></i></button>
+                                <button class="slide service-icon btn btn-primary" onclick="switchSrc(this, 'Jugete')"><i class="fa-solid fa-robot"></i></button>
+                                <!-- <button class="slide service-icon btn btn-primary" onclick="switchSrc(this, 'ToyTrain')">u</button>
+                                <button class="slide service-icon btn btn-primary" onclick="switchSrc(this, 'Canoe')">u</button> -->
+                            </div>
+                        </div>
+                        <script type="module">
+                        const modelViewer = document.querySelector("model-viewer");
+
+                        window.switchSrc = (element, name) => {
+                            const base = "view/assets/" + name;
+                            modelViewer.src = base + '.glb';
+                            const slides = document.querySelectorAll(".slide");
+                            slides.forEach((element) => {element.classList.remove("selected");});
+                            element.classList.add("selected");
+                        };
+
+                        document.querySelector(".slider").addEventListener('beforexrselect', (ev) => {
+                            // Keep slider interactions from affecting the XR scene.
+                            ev.preventDefault();
+                        });
+                        </script>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 my-5 my-lg-0">
+                        <div class="text-center p-5" style="background: rgba(51, 33, 29, .8);">
+                            <h1 class="text-white mb-4 mt-5">Modelos 3D</h1>
+                            <p class="text-white">Mi habilidad en el modelaje 3D se basa en el dominio de Blender, una aplicación de renombre que me permite dar vida a ideas y conceptos en entornos tridimensionales.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Modelo 3d End -->
+
+    <!-- Sitio web Start -->
+    <div class="container-fluid py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 py-0 py-lg-4">
+                    <h1 class="mb-3">Programación web</h1>
+                    <h5 class="mb-3">Mi pasión por el desarrollo web se refleja en mi experiencia con tecnologías clave como HTML, PHP y CSS, que utilizo para crear soluciones sólidas y eficientes en el backend.</h5>
+                    <p>Mi conjunto de habilidades abarca el desarrollo backend, donde destaco en la programación de la funcionalidad del sitio web</p>
+                </div>
+                <div class="col-lg-6 py-0 py-lg-4">
+                    <img src="view/img/Carrusel_5.png" class="img-fluid" alt="...">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Sitio web End -->
+
+    <!-- Unity Start -->
+    <div class="container-fluid pt-5">
+        <div class="container">
+            <div class="section-title">
+                <h1 class="mb-3">Programación Unity</h1>
+            </div>
+            <div class="row">
+                <div class="col-lg-6 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-sm-5">
+                            <img class="img-fluid mb-3 mb-sm-0" src="view/img/Modelo3D.png" alt="">
+                        </div>
+                        <div class="col-sm-7">
+                            <h4><i class="fa-solid fa-cube service-icon"></i>Modelado 3D</h4>
+                            <p class="m-0">Dentro del mundo de Unity, destaco en el modelado y animación 3D, creando elementos visuales impresionantes que enriquecen mis proyectos.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-sm-5">
+                            <img class="img-fluid mb-3 mb-sm-0" src="view/img/Programacion.png" alt="">
+                        </div>
+                        <div class="col-sm-7">
+                            <h4><i class="fa-brands fa-unity service-icon"></i>Programación C#</h4>
+                            <p class="m-0">Mi conocimiento sólido de C# es una herramienta fundamental para crear lógica interactiva y sistemas complejos en mis proyectos. Esto garantiza que mis aplicaciones sean tanto visualmente impactantes como altamente funcionales.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-sm-5">
+                            <img class="img-fluid mb-3 mb-sm-0" src="view/img/RealidadVirtual.png" alt="">
+                        </div>
+                        <div class="col-sm-7">
+                            <h4><i class="fa-solid fa-vr-cardboard service-icon"></i>Desarrollo en Realidad Virtual</h4>
+                            <p class="m-0">En la creación de aplicaciones de realidad virtual ofrezco una experiencias inmersivas. Desde la programación de interacciones hasta la creación de entornos similares a la realidad para el agrado de los usuairos.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-sm-5">
+                            <img class="img-fluid mb-3 mb-sm-0" src="view/img/Unity.png" alt="">
+                        </div>
+                        <div class="col-sm-7">
+                            <h4><i class="fa-solid fa-flag service-icon"></i>Vanguardia</h4>
+                            <p class="m-0">Siempre estoy en la búsqueda de nuevas técnicas y soluciones en el desarrollo de Unity. Mi dedicación a la investigación constante me permite mantener mis proyectos actualizados y optimizados.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Unity End -->
+
+    <!-- Footer Start -->
+    <div class="container-fluid footer text-white mt-5 pt-5 px-0 position-relative overlay-top" style="background: rgba(57, 49, 36, 1);">
+        <div class="container-fluid text-center text-white border-top mt-4 py-4 px-sm-3 px-md-5" style="border-color: rgba(256, 256, 256, .1) !important;">
+        <h4 class="text-primary text-uppercase mb-4" style="letter-spacing: 3px;">Contectame</h4>
+            <p><i class="fa fa-phone-alt mr-2"></i> 477 145 3895</p>
+            <p class="m-0"><i class="fa fa-envelope mr-2"></i>danielrmzmares@gmail.com</p>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary btn-lg-square back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="view/lib/easing/easing.min.js"></script>
+    <script src="view/lib/waypoints/waypoints.min.js"></script>
+    <script src="view/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="view/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="view/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="view/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="view/mail/jqBootstrapValidation.min.js"></script>
+    <script src="view/mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="view/js/main.js"></script>
+</body>
+
+</html>
